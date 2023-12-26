a985556e47d1fa27e2d2242243e3f21671d97bf8
Muhammad Awais <muhammad.awais@systempavers.com>
2023-12-27 00:04:13 +0500

    control flow added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./components/homepage/homepage.component').then((x) => x.HomepageComponent),
+  },
+  {
+    path: 'defer',
+    loadComponent: () => import('./components/homepage/homepage.component').then((x) => x.HomepageComponent),
+  },
+  {
+    path: 'control-flow',
+    loadComponent: () => import('./components/control-flow/control-flow.component').then((x) => x.ControlFlowComponent),
   }
 ];
 
--- src/app/components/control-flow/control-flow.component.ts
+++ src/app/components/control-flow/control-flow.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from 'src/app/shared/components/card/card.component';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.scss'
 })
 export class ControlFlowComponent {
+
+  isCard: boolean = false;
+
+  users: any = [
+    { id: 1, name: 'Muhammad' },
+    { id: 2, name: 'Awais' },
+    { id: 3, name: 'Ilyas' },
+  ]
+
+  toggleCard() {
+    this.isCard = !this.isCard;
+  }
 
 }
